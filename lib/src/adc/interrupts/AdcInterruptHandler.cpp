152bda402e1be3c39ff8b616c45a0d6339195ca9
Andy Brown <andy@andybrown.me.uk>
2014-02-10 20:44:35 +0000

    Optimise the adc interrupt handler. provide single conversion example.

--- lib/src/adc/interrupts/AdcInterruptHandler.cpp
+++ lib/src/adc/interrupts/AdcInterruptHandler.cpp
@@ -23,60 +23,67 @@ extern "C" {
 
     void __attribute__ ((interrupt("IRQ"))) ADC_IRQHandler(void) {
 
-      // ADC1 interrupts - the more likely to be a source. we'll prioritise these checks so
-      // the most likely are checked first.
+      uint16_t raised1,raised2,raised3;
 
-      if(ADC_GetITStatus(ADC1,ADC_IT_EOC)!=RESET) {
+      // ADC1 interrupts - the more likely to be a source. we'll prioritise these checks so
+      // the most likely are checked first and optimise slightly by not repeatedly calling
+      // the inefficient ADC_GetITStatus call
+
+      raised1=ADC1->SR & 0x27;
+      raised2=ADC2->SR & 0x27;
+      raised3=ADC3->SR & 0x27;
+
+      if((raised1 & 0x2)!=0) {
         AdcInterruptFeature::_adcInstance->AdcInterruptEventSender.raiseEvent(AdcEventType::EVENT_REGULAR_END_OF_CONVERSION,1);
         ADC_ClearITPendingBit(ADC1,ADC_IT_EOC);
       }
-      else if(ADC_GetITStatus(ADC1,ADC_IT_JEOC)!=RESET) {
+      else if((raised1 & 0x4)!=0) {
         AdcInterruptFeature::_adcInstance->AdcInterruptEventSender.raiseEvent(AdcEventType::EVENT_INJECTED_END_OF_CONVERSION,1);
         ADC_ClearITPendingBit(ADC1,ADC_IT_JEOC);
       }
-      else if(ADC_GetITStatus(ADC1,ADC_IT_AWD)!=RESET) {
+      else if((raised1 & 0x1)!=0) {
         AdcInterruptFeature::_adcInstance->AdcInterruptEventSender.raiseEvent(AdcEventType::EVENT_ANALOG_WATCHDOG,1);
         ADC_ClearITPendingBit(ADC1,ADC_IT_AWD);
       }
-      else if(ADC_GetITStatus(ADC1,ADC_IT_OVR)!=RESET) {
+      else if((raised1 & 0x20)) {
         AdcInterruptFeature::_adcInstance->AdcInterruptEventSender.raiseEvent(AdcEventType::EVENT_OVERFLOW,1);
         ADC_ClearITPendingBit(ADC1,ADC_IT_OVR);
       }
 
       // ADC2 interrupts
 
-      else if(ADC_GetITStatus(ADC2,ADC_IT_EOC)!=RESET) {
+      else if((raised2 & 0x2)!=0) {
         AdcInterruptFeature::_adcInstance->AdcInterruptEventSender.raiseEvent(AdcEventType::EVENT_REGULAR_END_OF_CONVERSION,2);
         ADC_ClearITPendingBit(ADC2,ADC_IT_EOC);
       }
-      else if(ADC_GetITStatus(ADC2,ADC_IT_JEOC)!=RESET) {
+      else if((raised2 & 0x4)!=0) {
         AdcInterruptFeature::_adcInstance->AdcInterruptEventSender.raiseEvent(AdcEventType::EVENT_INJECTED_END_OF_CONVERSION,2);
         ADC_ClearITPendingBit(ADC2,ADC_IT_JEOC);
       }
-      else if(ADC_GetITStatus(ADC2,ADC_IT_AWD)!=RESET) {
+      else if((raised2 & 0x1)!=0) {
         AdcInterruptFeature::_adcInstance->AdcInterruptEventSender.raiseEvent(AdcEventType::EVENT_ANALOG_WATCHDOG,2);
         ADC_ClearITPendingBit(ADC2,ADC_IT_AWD);
       }
-      else if(ADC_GetITStatus(ADC2,ADC_IT_OVR)!=RESET) {
+      else if((raised2 & 0x20)) {
         AdcInterruptFeature::_adcInstance->AdcInterruptEventSender.raiseEvent(AdcEventType::EVENT_OVERFLOW,2);
         ADC_ClearITPendingBit(ADC2,ADC_IT_OVR);
       }
 
       // ADC3 interrupts
 
-      else if(ADC_GetITStatus(ADC3,ADC_IT_EOC)!=RESET) {
+      else if((raised3 & 0x2)!=0) {
         AdcInterruptFeature::_adcInstance->AdcInterruptEventSender.raiseEvent(AdcEventType::EVENT_REGULAR_END_OF_CONVERSION,3);
         ADC_ClearITPendingBit(ADC3,ADC_IT_EOC);
       }
-      else if(ADC_GetITStatus(ADC3,ADC_IT_JEOC)!=RESET) {
+      else if((raised3 & 0x4)!=0) {
         AdcInterruptFeature::_adcInstance->AdcInterruptEventSender.raiseEvent(AdcEventType::EVENT_INJECTED_END_OF_CONVERSION,3);
         ADC_ClearITPendingBit(ADC3,ADC_IT_JEOC);
       }
-      else if(ADC_GetITStatus(ADC3,ADC_IT_AWD)!=RESET) {
+      else if((raised3 & 0x1)!=0) {
         AdcInterruptFeature::_adcInstance->AdcInterruptEventSender.raiseEvent(AdcEventType::EVENT_ANALOG_WATCHDOG,3);
         ADC_ClearITPendingBit(ADC3,ADC_IT_AWD);
       }
-      else if(ADC_GetITStatus(ADC3,ADC_IT_OVR)!=RESET) {
+      else if((raised3 & 0x20)) {
         AdcInterruptFeature::_adcInstance->AdcInterruptEventSender.raiseEvent(AdcEventType::EVENT_OVERFLOW,3);
         ADC_ClearITPendingBit(ADC3,ADC_IT_OVR);
       }
