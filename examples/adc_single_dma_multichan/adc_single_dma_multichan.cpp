bf6b4cec6833fa6020b728a99b09965d9a1114e7
Andy Brown <andy@andybrown.me.uk>
2014-02-12 13:49:47 +0000

    working on adc/dma example

--- examples/adc_single_dma_multichan/adc_single_dma_multichan.cpp
+++ examples/adc_single_dma_multichan/adc_single_dma_multichan.cpp
@@ -15,6 +15,20 @@ using namespace stm32plus;
 
 
 /**
+ * This example adds DMA and multi-channel conversion to the mix. We'll use ADC1 to convert
+ * three channels automatically and in sequence and we'll write out the results to the
+ * USART for you to see.
+ *
+ * The ADC is configured in 'scan mode' which means that it will convert all the configured
+ * channels and, because we are not using continuous mode, it will stop at the end of the
+ * group. The DMA channel for ADC1 is used to move the converted channel data out to SRAM.
+ * We configure the end-of-conversion interrupt (EOC) to fire when the complete group has
+ * finished converting. The converted data is written to the USART, we pause for a second
+ * and then do it all again, ad infinitum.
+ *
+ * USART1 is configured with protocol settings of 57600/8/N/1. The ADC channels are read
+ * from PA[0], PA[1], PA[2]. You will need to connect these GPIO inputs to valid levels
+ * between GND and VREF to see conversion values.
  *
  * Compatible MCU:
  *   STM32F0
@@ -30,9 +44,21 @@ using namespace stm32plus;
 
 class AdcSingleDmaMultiChan {
 
+  private:
+    volatile bool _ready;
+
   public:
 
     void run() {
+
+      _ready=false;
+
+      /*
+       * We're converting 3 channels in circular buffer mode so we need exactly 3 16-bit words
+       * for that circular buffer
+       */
+
+      uint16_t readBuffer[3];
 
       /*
        * Declare the ADC peripheral with an APB2 clock prescaler of 2, a resolution of
@@ -44,6 +70,7 @@ class AdcSingleDmaMultiChan {
         AdcClockPrescalerFeature<2>,              // prescaler of 2
         AdcResolutionFeature<12>,                 // 12 bit resolution
         Adc1Cycle3RegularChannelFeature<0,1,2>,   // using channels 0,1 and 2 on ADC1 with 3-cycle latency
+        AdcScanModeFeature<AdcScanModeEndType::END_AFTER_WHOLE_GROUP>,    // scan mode with end interrupt after each group
         Adc1InterruptFeature                      // enable interrupt handling on this ADC
       > adc;
 
@@ -72,10 +99,11 @@ class AdcSingleDmaMultiChan {
       UsartPollingOutputStream outputStream(usart);
 
       /**
-       * Enable the ADC interrupt
+       * Enable the ADC interrupt and start the DMA
        */
 
       adc.enableInterrupts(ADC_IT_EOC);
+      dma.beginRead(readBuffer,sizeof(readBuffer));
 
       /*
        * Go into an infinite loop converting
@@ -83,34 +111,49 @@ class AdcSingleDmaMultiChan {
 
       for(;;) {
 
-        uint16_t value;
+        /*
+         * For this test code we set the 3 words to a known pattern so we can tell if they
+         * get properly overwritten by the DMA during conversion
+         */
 
-        // start a conversion and wait until the peripheral signals that it's started
+        readBuffer[0]=readBuffer[1]=readBuffer[2]=0xAAAA;
+
+        /*
+         * Start a conversion and wait until the interrupt handler tells us
+         * that it's finished.
+         */
 
         adc.startRegularConversion();
-        while(!adc.hasRegularConversionStarted());
 
-        // poll the EOC flag that tells us when the conversion has finished
+        while(!_ready);
+        _ready=false;
 
-        while(!adc.hasRegularConversionFinished());
+        /*
+         * write the 3 values to the USART
+         */
 
-        // get the result
+        outputStream << "Converted values are "
+                     << StringUtil::Ascii(readBuffer[0]) << ", "
+                     << StringUtil::Ascii(readBuffer[1]) << ", "
+                     << StringUtil::Ascii(readBuffer[2]) << "\r\n";
 
-        value=adc.getRegularConversionValue();
-
-        // write the value to the USART
-
-        outputStream << "Converted value is " << StringUtil::Ascii(value) << "\r\n";
-
-        // wait for a second before converting the next value
+        // wait for a second before converting the next set of values
 
         MillisecondTimer::delay(1000);
       }
     }
 
 
+    /**
+     * Interrupt handler for the EOC event. If the event and the ADC number
+     * match then we set the ready flag. This will release the main code
+     * that's waiting on it.
+     */
+
     void onInterrupt(AdcEventType eventType,uint8_t adcNumber) {
+
       if(eventType==AdcEventType::EVENT_REGULAR_END_OF_CONVERSION && adcNumber==1) {
+        _ready=true;
       }
     }
 };
--- lib/include/adc/features/AdcScanModeFeature.h
+++ lib/include/adc/features/AdcScanModeFeature.h
@@ -10,13 +10,37 @@
 namespace stm32plus {
 
   /**
-   * Trivial feature class to enable scan-mode
+   * This enum determines when the EOC flag is set during scan mode. END_AFTER_EACH_CHANNEL
+   * means that EOC gets raised after each channel in the group is converted. So there will
+   * be N EOC's for a group containing N channels. END_AFTER_WHOLE_GROUP means that only a
+   * single EOC flag will be raised for each group of configured channels. This mode normally
+   * implies that you'll be using DMA to move data out to SRAM after each channel is converted.
    */
 
+  enum class AdcScanModeEndType : uint8_t {
+    END_AFTER_EACH_CHANNEL,
+    END_AFTER_WHOLE_GROUP
+  };
+
+
+  /**
+   * Feature class to enable scan-mode. The scan mode end type is a template parameter so
+   * that we can generate zero conditional code for the ADC_EOCOnEachRegularChannelCmd parameter.
+   * @tparam TEndType The desired condition for raising the EOC flag
+   */
+
+  template<AdcScanModeEndType TEndType>
   struct AdcScanModeFeature : AdcFeatureBase {
 
     AdcScanModeFeature(Adc& adc)
       : AdcFeatureBase(adc) {
+
+      static_assert(
+          TEndType==AdcScanModeEndType::END_AFTER_EACH_CHANNEL ||
+          TEndType==AdcScanModeEndType::END_AFTER_WHOLE_GROUP,
+          "TEndType is not a member of the AdcScanModeEndType enumeration");
+
+      ADC_EOCOnEachRegularChannelCmd(adc,TEndType==AdcScanModeEndType::END_AFTER_EACH_CHANNEL ? ENABLE : DISABLE);
       ((ADC_InitTypeDef *)adc)->ADC_ScanConvMode=ENABLE;
     }
   };
