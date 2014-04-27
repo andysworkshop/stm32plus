026b1f345aed7941db89e3a9663f8b1f96d401b8
Andy Brown <andy@andybrown.me.uk>
2014-04-27 08:45:31 +0100

    gpio out is not required on timer feature

--- examples/adc_single_timer_interrupts/adc_single_timer_interrupts.cpp
+++ examples/adc_single_timer_interrupts/adc_single_timer_interrupts.cpp
@@ -57,11 +57,7 @@ class AdcSingleTimerInterrupts {
 
       Timer2<
         Timer2InternalClockFeature,       // the timer clock source is HCLK/2
-        TimerChannel1Feature,             // we're going to use channel 1
-        Timer2GpioFeature<                // we want to output something to GPIO
-          TIMER_REMAP_NONE,               // the GPIO output will not be remapped
-          TIM2_CH1_OUT                    // we will output channel 1 to GPIO
-        >
+        TimerChannel1Feature              // we're going to use channel 1
       > timer;
 
       /*
