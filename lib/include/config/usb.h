9af77138a917becb0156aae11cb892655510f40c
Andy Brown <andy@andybrown.me.uk>
2014-11-03 13:20:20 +0000

    begin integrate usb hid/device core

---
+++ lib/include/config/usb.h
@@ -0,0 +1,28 @@
+/*
+ * This file is a part of the open source stm32plus library.
+ * Copyright (c) 2011,2012,2013,2014 Andy Brown <www.andybrown.me.uk>
+ * Please see website for licensing terms.
+ */
+
+#pragma once
+
+/**
+ * @file
+ * Include this config file to get access to the USB peripheral.
+ */
+
+// usb depends on ...
+
+#include "config/rcc.h"
+#include "config/gpio.h"
+#include "config/dma.h"
+#include "config/stream.h"
+#include "config/event.h"
+
+// device-specific pin initialiser
+
+#if defined(STM32PLUS_F4)
+
+
+#endif
+
