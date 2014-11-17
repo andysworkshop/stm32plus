ac5de6cf03519e4c9bc9333444060b5030f4180e
Andy Brown <andy@andybrown.me.uk>
2014-11-17 13:26:40 +0000

    start work on LIS302DL demo

--- examples/usb_device_hid_mouse/usb_device_hid_mouse.cpp
+++ examples/usb_device_hid_mouse/usb_device_hid_mouse.cpp
@@ -6,7 +6,7 @@
 
 #include "config/stm32plus.h"
 #include "config/usb.h"
-#include "config/button.h"
+#include "config/spi.h"
 
 
 using namespace stm32plus;
@@ -14,18 +14,35 @@ using namespace stm32plus::usb;
 
 
 /**
+ * This example demonstrates using the accelerometer in your STM32F4DISCOVERY board as a mouse
+ * by implementing the USB Human interface device (HID) protocol. The MouseHidDevice class is
+ * used to implement the USB mouse boot protocol that requires no PC driver installation.
+ *
+ * By tilting your discovery board in the X,Y directions you will, amusingly, be able to move
+ * your computer mouse pointer around the screen. Sometimes it might even move in the direction
+ * you intend it to...
+ *
+ * To use this example, compile it and flash it to your STM32F4DISCOVERY board. Attach a USB
+ * cable from the micro-USB socket on the discovery board to your PC. The USB device should be
+ * recognised automatically. Have fun with your mouse!
+ *
+ * Compatible MCU:
+ *   STM32F4
+ *
+ * Tested on devices:
+ *   STM32F407VGT6
  */
 
 class UsbDeviceHidMouseTest {
 
   public:
 
-    // declare the USB stack
+    /*
+     * declare a type for the the USB stack
+     */
 
     typedef MouseHidDevice<
-      InternalFsPhy<                      // use the internal full speed PHY
-        FsLowPowerFeature                 // include the low power EXTI wakeup
-      >,
+      InternalFsPhy<>,                    // use the internal full speed PHY with no features
       ManufacturerTextFeature,            // we'll supply a manufacturer name
       ProductTextFeature,                 // ... and a product name
       SerialNumberTextFeature,            // ... and a serial number
@@ -33,47 +50,81 @@ class UsbDeviceHidMouseTest {
       InterfaceTextFeature                // ... and an interface text string
     > MyUsb;
 
+    /*
+     * Declare the SPI pins. This is almost the same as the default SPI1 except that
+     * ST moved NSS (chip-select) to PE3
+     */
+
+    enum {
+      Port_NSS=GPIOE_BASE,
+      Port_SCK=GPIOA_BASE,
+      Port_MISO=GPIOA_BASE,
+      Port_MOSI=GPIOA_BASE,
+
+      Pin_NSS  = GPIO_Pin_3,
+      Pin_SCK  = GPIO_Pin_5,
+      Pin_MISO = GPIO_Pin_6,
+      Pin_MOSI = GPIO_Pin_7
+    };
+
+    /*
+     * Declare a SPI type
+     */
+
+    typedef Spi1_Custom<UsbDeviceHidMouseTest> MySpi;
+
+    /*
+     * Run the example
+     */
+
     void run() {
 
-      MyUsb::Parameters params;
+      /*
+       * set up the parameters for the USB hid device
+       */
 
-      params.device_vid=0xDEAD;
-      params.device_pid=0xBEEF;
-      params.device_manufacturer_text="Andy's Workshop";   // see params.device_language_[ids/count] to change the languages
-      params.device_product_text="stm32plus";
-      params.device_serial_text="0123456789";
-      params.device_configuration_text="My configuration";
-      params.device_interface_text="My interface";
+      MyUsb::Parameters usbParams;
+
+      usbParams.device_vid=0xDEAD;           // demo vendor ID
+      usbParams.device_pid=0xBEEF;           // demo product ID
+      usbParams.device_manufacturer_text="Andy's Workshop";   // see params.device_language_[ids/count] to change the languages
+      usbParams.device_product_text="stm32plus wacky mouse";
+      usbParams.device_serial_text="0123456789";
+      usbParams.device_configuration_text="My configuration";
+      usbParams.device_interface_text="My interface";
+
+      /*
+       * Declare the USB object - this will initialise internal variables but will not
+       * start the peripheral
+       */
 
       MyUsb usb;
 
-      // there is no requirement for the parameters to remain in scope after the initialise call
+      /*
+       * Start the USB peripheral. It will run asynchronously. There is no requirement
+       * for the parameters to remain in scope after the initialise call
+       */
 
-      if(!usb.initialise(params))
+      if(!usb.initialise(usbParams))
         error();
 
-      GpioA<DefaultDigitalInputFeature<0> > pa;
+      /*
+       * The USB device is now up and running. Now we initialise the LIS302DL SPI1 interface
+       */
 
-      // create the button class with parameters
+      MySpi::Parameters spiParams;
 
-      AutoRepeatPushButton button(pa[0],false,600,150);
+      // override some of the default SPI parameters
 
-      // main loop
+      spiParams.spi_baudRatePrescaler=SPI_BaudRatePrescaler_4;
+      spiParams.spi_cpol=SPI_CPOL_Low;
+      spiParams.spi_cpha=SPI_CPHA_1Edge;
 
-      for(;;) {
+      MySpi spi(spiParams);
 
-        if(button.getState()==PushButton::Pressed) {
+      // the LIS302DL interrupt1 and interrupt2 pins are on PE0, PE1,
 
-          uint8_t buffer[4];
-
-          buffer[0] = 0;
-          buffer[1] = 1;
-          buffer[2] = 0;
-          buffer[3] = 0;
-
-          usb.hidSendReport(buffer);
-        }
-      }
+      GpioE<DefaultDigitalInputFeature<0,1>> pe;
     }
 
 
