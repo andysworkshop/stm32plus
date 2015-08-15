c7f07e7bc25bd3509674d446946c09696058225c
Andy Brown <andy@andybrown.me.uk>
2015-08-15 11:50:08 +0100

    restore ST's method of defaulting HSE_VALUE when you don't set it. #129

--- lib/fwlib/f4/cmsis/Device/ST/STM32F4xx/Include/stm32f4xx.h
+++ lib/fwlib/f4/cmsis/Device/ST/STM32F4xx/Include/stm32f4xx.h
@@ -75,6 +75,16 @@
   #define HSE_STARTUP_TIMEOUT    ((uint16_t)0x05000)   /*!< Time out for HSE start up */
 #endif /* HSE_STARTUP_TIMEOUT */
 
+#if defined(STM32F40_41xxx) || defined(STM32F427_437xx)  || defined(STM32F429_439xx) || defined(STM32F401xx)  || defined(STM32F411xE)
+ #if !defined  (HSE_VALUE)
+  #define HSE_VALUE    ((uint32_t)25000000) /*!< Value of the External oscillator in Hz */
+#endif /* HSE_VALUE */
+#elif defined(STM32F446xx)
+ #if !defined  (HSE_VALUE)
+  #define HSE_VALUE    ((uint32_t)8000000) /*!< Value of the External oscillator in Hz */
+#endif /* HSE_VALUE */
+#endif /* STM32F40_41xxx || STM32F427_437xx || STM32F429_439xx || STM32F401xx || STM32F411xE */
+
 
 /** @addtogroup Library_configuration_section
   * @{
