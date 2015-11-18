950f64f79a785f7c30c67e7de3c3e11223e586df
Andy Brown <andy@andybrown.me.uk>
2015-11-18 19:31:04 +0000

    move releaseJtagPinsForGpio to GpioPortBase. #139

--- lib/include/gpio/f1/GpioPort.h
+++ lib/include/gpio/f1/GpioPort.h
@@ -53,17 +53,6 @@ namespace stm32plus {
         return (GPIO_TypeDef *)TPortAddress;
       }
 
-
-      /**
-       * Release PA15,PB3,PB4
-       */
-
-      void releaseJtagPinsForGpio() {
-        RCC_APB2PeriphClockCmd(RCC_APB2ENR_AFIOEN,ENABLE);
-        GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable,ENABLE);
-      }
-
-
     public:
 
       /**
